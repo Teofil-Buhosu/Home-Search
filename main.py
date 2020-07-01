0471ffc9e285158ae338d3366ccbb91caf1b1902
abuhosu <Teofil.Buhosu@pm.me>
2020-07-01 18:12:49 +0200

    inital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+if __name__ == "__main__":
+    browser = webdriver.Chrome()
+    browser.get('https://casa.it')
+    # get user search env with selenium
+    # get list of homes
+    # ask for user filters
+    # sort homes
+    # output an exel
